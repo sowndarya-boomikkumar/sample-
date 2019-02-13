0c79077d65b4bc32552048c512e4da9501affe7c
sowndarya-boomikkumar <43332541+sowndarya-boomikkumar@users.noreply.github.com>
2019-02-13 14:22:25 +0530

    Create oddreven.c

---
+++ oddreven.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+#include<conio.h>
+void main()
+{
+printf("enter the number a:");
+scanf("%d",&a);
+if(a/2==0)
+{
+printf("%d is even");
+}
+else
+{
+printf('%d is odd");
+}
+getch();
+}
