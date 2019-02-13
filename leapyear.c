f89b8d207de41a4dfe7c340aa2d0820974b418a4
sowndarya-boomikkumar <43332541+sowndarya-boomikkumar@users.noreply.github.com>
2019-02-13 14:56:29 +0530

    Create leapyear.c

---
+++ leapyear.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+#include<conio.h.
+void main()
+{
+int A;
+printf("enter the year A:");
+sacanf("%d",&A);
+if(A % 400==0)
+{
+printf("the entered year is leap year");
+}
+else
+{
+printf("the entered year is not a leap year")
+}
+getch();
+}
