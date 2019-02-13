609462936ed6af828728ac24acdaa12be9401ec0
sowndarya-boomikkumar <43332541+sowndarya-boomikkumar@users.noreply.github.com>
2019-02-13 14:49:04 +0530

    Create largest.c

---
+++ largest.c
@@ -0,0 +1,24 @@
+#include<stdio.h>
+#include<conio.h>
+void main()
+{
+int a,,b,c;
+printf("enter the number a:");
+scanf("%d",&a);
+printf("enter the number b:");
+scanf("%d",&b);
+printf("enter the number c:");
+scanf("%d",&c);
+if (a>=b && a >= c)
+{
+printf("%d is the largest number.", a); 
+  }
+ else if (b >= a && b >= c) 
+ 
+{       
+ printf("%d is the largest number.", b; 
+  }
+ else (c >= a && c  >= b) 
+ printf("%d is the largest number.",c); 
+ } 
+getch();
